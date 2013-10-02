79ccf2910d28fb69f1b3529999d2c35c335a5c6a
Guillaume 'Llew' Delpierre <gde@llew.me>
2013-10-02 21:26:41 +0200

    fix variable

--- nuage/predicate.py
+++ nuage/predicate.py
@@ -28,13 +28,13 @@
 class LoggedPredicate(object):
     """
     """
-    phash = text
-
     def __init__(self, val, config):
         self._val = val
 
     def text(self):
         return "predicat on login"
+
+    phash = text
 
     def __call__(self, context, request):
         return ('login' in request.session) == self._val
