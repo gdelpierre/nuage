49c7bc7b2bba891f726477d2443659b359eb472f
Llew <gde@nbs-system.com>
2013-10-02 15:34:04 +0200

    rename all occurence cloud to nuage

--- nuage/views.py
+++ nuage/views.py
@@ -3,4 +3,4 @@ from pyramid.view import view_config
 
 @view_config(route_name='home', renderer='templates/home.pt')
 def my_view(request):
-    return {'project': 'cloud'}
+    return {'project': 'nuage'}
--- setup.py
+++ setup.py
@@ -28,9 +28,9 @@ requires = [
     'pyramid_mailer',
     ]
 
-setup(name='cloud',
+setup(name='nuage',
       version='0.0',
-      description='cloud',
+      description='nuage',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
         "Programming Language :: Python",
@@ -41,15 +41,15 @@ setup(name='cloud',
       author=u'Guillaume Delpierre',
       author_email='gde+code@llew.me',
       url='',
-      keywords='cloud pyramid',
+      keywords='nuage pyramid',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
       install_requires=requires,
       tests_require=requires,
-      test_suite="cloud",
+      test_suite="nuage",
       entry_points = """\
       [paste.app_factory]
-      main = cloud:main
+      main = nuage:main
       """,
       )
