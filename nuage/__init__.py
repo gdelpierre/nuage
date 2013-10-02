98176bf3cbf0ad7c82ad67c3141a3c446fa19ef4
Guillaume 'Llew' Delpierre <gde@llew.me>
2013-10-02 21:15:12 +0200

    add def home,signup,login in __init__

--- nuage/__init__.py
+++ nuage/__init__.py
@@ -1,12 +1,57 @@
-from pyramid.config import Configurator
+############################################################################
+# The MIT License (MIT)
+#
+# Copyright (c) 2013 Guillaume Delpierre
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNES FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+############################################################################
 
+""" Nuage
+"""
+
+from pyramid.config import Configurator
+from pyramid_beaker import session_factory_from_settings
+from pyramid_beaker import set_cache_regions_from_settings
+
+from pyramid.threadlocal import get_current_registry
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     config = Configurator(settings=settings)
-    config.include('pyramid_chameleon')
+
+    get_current_registry().settings = settings
+    
     config.add_static_view('static', 'static', cache_max_age=3600)
+
     config.add_route('home', '/')
+    config.add_route('submitLogin', '/home/submit')
+
+    config.add_route('signup', '/signup')
+    config.add_route('submitSignup', '/signup/submit')
+
+    for include in ['pyramid_chameleon', \
+                    'pyramid_fanstatic', \
+                    'pyramid_mailer', \
+                    'pyramid_beaker', \
+                    ]:
+        config.include(include)
+
     config.scan()
     return config.make_wsgi_app()
